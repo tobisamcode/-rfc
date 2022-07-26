192db9852b7f5447a397ac6bc5012dea7a16f565
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-07-26 19:17:59 +0100

    first push

--- src/Content.js
+++ src/Content.js
@@ -1,17 +1,76 @@
+import { useState } from "react";
+import { FaTrashAlt } from "react-icons/fa";
+
 const Content = () => {
-    const handleNameChange = () => {
-        const names = ['Bob', 'Kevin', 'Dave'];
-        const int = Math.floor(Math.random() * 3);
-        return names[int];
+  const [items, setItems] = useState([
+    {
+      id: 1,
+      checked: false,
+      item: "One half pound bag of Cocoa Covered Almonds Unsalted"
+    },
+    {
+      id: 2,
+      checked: true,
+      item: "item 2"
+    },
+    {
+      id: 3,
+      checked: false,
+      item: "item 2"
     }
+  ]);
 
-    return (
-        <main>
-            <p>
-                Hello {handleNameChange()}!
-            </p>
-        </main>
-    )
-}
+  const handleCheck = id => {
+    const listItems = items.map(
+      item => (item.id === id ? { ...item, checked: !item.checked } : item)
+    );
+    console.log(listItems);
+    setItems(listItems);
+    localStorage.setItem("shoppinglist", JSON.stringify(listItems));
+  };
 
-export default Content
+  const handleDelete = id => {
+    const listItems = items.filter(item => item.id !== id);
+    setItems(listItems);
+    localStorage.setItem("shoppinglist", JSON.stringify(listItems));
+  };
+
+  return (
+    <main>
+      {items.length
+        ? <ul>
+            {items.map(item =>
+              <li className="item" key={item.id}>
+                <input
+                  onChange={() => handleCheck(item.id)}
+                  type="checkbox"
+                  checked={item.checked}
+                />
+
+                <label
+                  style={
+                    item.checked
+                      ? {
+                          textDecoration: "line-through"
+                        }
+                      : null
+                  }
+                  onDoubleClick={() => handleCheck(item.id)}
+                >
+                  {item.item}
+                </label>
+
+                <FaTrashAlt
+                  onClick={() => handleDelete(item.id)}
+                  role="button"
+                  tabIndex="0"
+                />
+              </li>
+            )}
+          </ul>
+        : "no items founds"}
+    </main>
+  );
+};
+
+export default Content;
