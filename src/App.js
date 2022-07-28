b53eb6946d859cfb32268e726013f11a8ab87e8e
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-07-29 00:56:03 +0100

    react

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import Header from './Header';
-import Content from './Content';
-import Footer from './Footer';
+import Header from "./Header";
+import Content from "./Content";
+import Footer from "./Footer";
 
 function App() {
-
   return (
     <div className="App">
       <Header />
