f4f53d68eeb1f3d1698a0e9d7165ad8ceeb7a7e1
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-07-29 01:13:54 +0100

    react

--- src/Content.js
+++ src/Content.js
@@ -1,39 +1,14 @@
-import { FaTrashAlt } from "react-icons/fa";
+import ItemList from "./ItemList";
 
 const Content = ({ items, handleCheck, handleDelete }) => {
   return (
     <main>
       {items.length
-        ? <ul>
-            {items.map(item =>
-              <li className="item" key={item.id}>
-                <input
-                  onChange={() => handleCheck(item.id)}
-                  type="checkbox"
-                  checked={item.checked}
-                />
-
-                <label
-                  style={
-                    item.checked
-                      ? {
-                          textDecoration: "line-through"
-                        }
-                      : null
-                  }
-                  onDoubleClick={() => handleCheck(item.id)}
-                >
-                  {item.item}
-                </label>
-
-                <FaTrashAlt
-                  onClick={() => handleDelete(item.id)}
-                  role="button"
-                  tabIndex="0"
-                />
-              </li>
-            )}
-          </ul>
+        ? <ItemList
+            items={items}
+            handleCheck={handleCheck}
+            handleDelete={handleDelete}
+          />
         : "no items founds"}
     </main>
   );
