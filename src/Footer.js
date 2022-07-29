4e115f2e39b375d670adde85052b8a079d06dcc5
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-07-29 01:09:48 +0100

    react

--- src/Footer.js
+++ src/Footer.js
@@ -1,11 +1,11 @@
-const Footer = () => {
-    const today = new Date();
+const Footer = ({ length }) => {
+  return (
+    <footer>
+      <p>
+        {length} list {length === 1 ? "item" : "items"}
+      </p>
+    </footer>
+  );
+};
 
-    return (
-        <footer>
-            <p>Copyright &copy; {today.getFullYear()}</p>
-        </footer>
-    )
-}
-
-export default Footer
+export default Footer;
