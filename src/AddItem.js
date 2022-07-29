c4c3270f17c5202dee61fda71a54cca16f10d8a2
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-07-29 03:31:51 +0100

    react

---
+++ src/AddItem.js
@@ -0,0 +1,22 @@
+import { FaPlus } from "react-icons/fa";
+
+const AddItem = ({ newItem, setNewItem, handleSubmit }) => {
+  return (
+    <form action="" className="addForm" onSubmit={handleSubmit}>
+      <label htmlFor="addItem">Add Item</label>
+      <input
+        type="text"
+        id="addItem"
+        placeholder="Add Item"
+        required
+        value={newItem}
+        onChange={e => setNewItem(e.target.value)}
+      />
+      <button type="submit" aria-label="Add Item">
+        <FaPlus />
+      </button>
+    </form>
+  );
+};
+
+export default AddItem;
