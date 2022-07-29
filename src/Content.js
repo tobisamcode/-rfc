1bfb12213f00c09732e3edb7ab5a30c4c30e2eed
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-07-29 01:09:44 +0100

    react

--- src/Content.js
+++ src/Content.js
@@ -1,40 +1,6 @@
-import { useState } from "react";
 import { FaTrashAlt } from "react-icons/fa";
 
-const Content = () => {
-  const [items, setItems] = useState([
-    {
-      id: 1,
-      checked: false,
-      item: "One half pound bag of Cocoa Covered Almonds Unsalted"
-    },
-    {
-      id: 2,
-      checked: true,
-      item: "item 2"
-    },
-    {
-      id: 3,
-      checked: false,
-      item: "item 2"
-    }
-  ]);
-
-  const handleCheck = id => {
-    const listItems = items.map(
-      item => (item.id === id ? { ...item, checked: !item.checked } : item)
-    );
-    console.log(listItems);
-    setItems(listItems);
-    localStorage.setItem("shoppinglist", JSON.stringify(listItems));
-  };
-
-  const handleDelete = id => {
-    const listItems = items.filter(item => item.id !== id);
-    setItems(listItems);
-    localStorage.setItem("shoppinglist", JSON.stringify(listItems));
-  };
-
+const Content = ({ items, handleCheck, handleDelete }) => {
   return (
     <main>
       {items.length
