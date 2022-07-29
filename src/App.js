ed09419fd36e87c7057f269a5b379cf2127b39a8
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-07-29 01:09:39 +0100

    react

--- src/App.js
+++ src/App.js
@@ -1,13 +1,51 @@
+import { useState } from "react";
 import Header from "./Header";
 import Content from "./Content";
 import Footer from "./Footer";
 
 function App() {
+  const [items, setItems] = useState([
+    {
+      id: 1,
+      checked: false,
+      item: "One half pound bag of Cocoa Covered Almonds Unsalted"
+    },
+    {
+      id: 2,
+      checked: true,
+      item: "item 2"
+    },
+    {
+      id: 3,
+      checked: false,
+      item: "item 2"
+    }
+  ]);
+
+  const handleCheck = id => {
+    const listItems = items.map(
+      item => (item.id === id ? { ...item, checked: !item.checked } : item)
+    );
+    console.log(listItems);
+    setItems(listItems);
+    localStorage.setItem("shoppinglist", JSON.stringify(listItems));
+  };
+
+  const handleDelete = id => {
+    const listItems = items.filter(item => item.id !== id);
+    setItems(listItems);
+    localStorage.setItem("shoppinglist", JSON.stringify(listItems));
+  };
+
   return (
     <div className="App">
-      <Header />
-      <Content />
-      <Footer />
+      <Header title="Groceries List" />
+      <Content
+        items={items}
+        handleCheck={handleCheck}
+        handleDelete={handleDelete}
+      />
+      <Footer length={items.length} />
     </div>
   );
 }
