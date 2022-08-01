14f8852812483a10120b89077c37b6bda52ec89d
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-08-01 15:05:03 +0100

    hooks

--- src/App.js
+++ src/App.js
@@ -75,9 +75,17 @@ function App() {
   };
 
   // delete items
-  const handleDelete = id => {
+  const handleDelete = async id => {
     const listItems = items.filter(item => item.id !== id);
     setItems(listItems);
+
+    const deleteOptions = {
+      method: "DELETE"
+    };
+    const reqUrl = `${API_URL}/${id}`;
+
+    const result = await apiRequest(reqUrl, deleteOptions);
+    if (result) setFetchError(result);
   };
 
   const handleSubmit = e => {
