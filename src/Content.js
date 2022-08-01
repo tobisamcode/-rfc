44cbdeb842d9248e04453ee017f4fe4f8f52f216
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-08-01 13:33:09 +0100

    hooks

--- src/Content.js
+++ src/Content.js
@@ -2,7 +2,7 @@ import ItemList from "./ItemList";
 
 const Content = ({ items, handleCheck, handleDelete }) => {
   return (
-    <main>
+    <>
       {items.length
         ? <ItemList
             items={items}
@@ -10,7 +10,7 @@ const Content = ({ items, handleCheck, handleDelete }) => {
             handleDelete={handleDelete}
           />
         : "no items founds"}
-    </main>
+    </>
   );
 };
 
