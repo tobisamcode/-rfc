ec749d07a34fe9ed3c18f614804f163e7c390fde
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-08-01 15:01:27 +0100

    hooks

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Content from "./Content";
 import Footer from "./Footer";
 import AddItem from "./AddItem";
 import SearchItem from "./SearchItem";
+import apiRequest from "./apiRequest";
 
 function App() {
   const API_URL = "http://localhost:3500/items";
@@ -34,20 +35,46 @@ function App() {
     }, 2000);
   }, []);
 
-  const addItem = item => {
+  // create item
+  const addItem = async item => {
     const id = items.length ? items[items.length - 1].id + 1 : 1;
     const myNewItem = { id, checked: false, item };
     const listItems = [...items, myNewItem];
     setItems(listItems);
+
+    const postOptions = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(myNewItem)
+    };
+    const result = await apiRequest(API_URL, postOptions);
+    if (result) fetchError(result);
   };
 
-  const handleCheck = id => {
+  // update item
+  const handleCheck = async id => {
     const listItems = items.map(
       item => (item.id === id ? { ...item, checked: !item.checked } : item)
     );
     setItems(listItems);
+
+    const myItem = listItems.filter(item => item.id === id);
+
+    const updateOptions = {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ checked: myItem[0].checked })
+    };
+    const reqUrl = `${API_URL}/${id}`;
+    const result = await apiRequest(reqUrl, updateOptions);
+    if (result) setFetchError(result);
   };
 
+  // delete items
   const handleDelete = id => {
     const listItems = items.filter(item => item.id !== id);
     setItems(listItems);
